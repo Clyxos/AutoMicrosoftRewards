0a47e7f79cb5eeb175be8714885c5d20fb797ae8
Clyxos <clyxosisgamer@gmail.com>
2023-03-09 22:07:55 -0500

    Update AutoMicrosoftrewards.py

--- AutoMicrosoftrewards.py
+++ AutoMicrosoftrewards.py
@@ -1,32 +1,30 @@
 import pyautogui as p
-import string as st
-import random as r
-import time as t
-firstsearches=0
-searches=int(0)
-st.ascii_letters
-'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'
+from string import ascii_letters as st
+from random import choices as r
+from time import sleep as t
+firstsearches = 0
+searches = int(0)
 # This next part of the code is the actual part that types the things into the browser
 p.press("win")
 p.typewrite("Microsoft Edge")
 p.press("Enter")
-t.sleep(2)
+t(2)
 p.click()
 print("Are you ready to begin? Press 1 if so, and put your mouse over the MS edge window.")
-temp=input("press 1 when ready ")
-t.sleep(2.5)
+temp = input("press 1 when ready ")
+t(2.5)
 while searches <= 34:
-    newletter=r.choices(st.ascii_letters)
+    newletter = r(st)
     if searches == 0:
-        type=newletter
+        type = newletter
     else:
-        type=newletter+existing
+        type = newletter+existing
     p.hotkey("ctrl","e")
     p.typewrite(type)
     p.typewrite(["enter"])
     searches += int(1)
     print(searches)
-    t.sleep(1)
+    t(1)
     existing=type
 else:
     quit("Done! Make sure you got all of your points.")
