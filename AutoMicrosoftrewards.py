fc15ee427f6688ea22917dfc684a5781832ad804
Clyxos <97849601+Clyxos@users.noreply.github.com>
2023-06-22 22:48:59 -0400

    Fixed library sizes to be reduced to be smaller.

--- AutoMicrosoftrewards.py
+++ AutoMicrosoftrewards.py
@@ -1,15 +1,15 @@
-from pyautogui import press, typewrite, click, hotkey as p
+from pyautogui import press, typewrite, click, hotkey
 from string import ascii_letters as st
 from random import choices as r
 from time import sleep as t
 firstsearches = 0
 searches = int(0)
 # This next part of the code is the actual part that types the things into the browser
-p.press("win")
-p.typewrite("Microsoft Edge")
-p.press("Enter")
+press("win")
+typewrite("Microsoft Edge")
+press("Enter")
 t(2)
-p.click()
+click()
 print("Are you ready to begin? Press 1 if so, and put your mouse over the MS edge window.")
 temp = input("press 1 when ready ")
 t(2.5)
@@ -19,13 +19,12 @@ while searches <= 34:
         type = newletter
     else:
         type = newletter+existing
-    p.hotkey("ctrl","e")
-    p.typewrite(type)
-    p.typewrite(["enter"])
+    hotkey("ctrl", "e")
+    typewrite(type)
+    typewrite(["enter"])
     searches += int(1)
     print(searches)
     t(1)
-    existing=type
+    existing = type
 else:
     quit("Done! Make sure you got all of your points.")
-    
